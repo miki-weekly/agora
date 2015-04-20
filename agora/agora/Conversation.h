80731440b85ef58dfaa93e27adaac1aeef3db16e
Tony Chen <tonychen@Tonys-Mac.local>
2015-04-19 17:49:16 -0700

    Parse now keeps track of unread messages of sender and recipient.  Also wrote function that gets total unread messages.

--- agora/agora/Conversation.h
+++ agora/agora/Conversation.h
@@ -13,7 +13,8 @@
 @interface Conversation : NSObject
 
 @property NSString* objectId;
-@property PFUser *recipient;
-@property Post *post;
+@property PFUser* recipient;
+@property Post* post;
+@property NSNumber* unread;
 
 @end
